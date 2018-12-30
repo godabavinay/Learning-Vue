7ccfa0e433f4fb57dbbe7f52074c14a440b0397e
CompositionInJava <godabavinay@gmail.com>
2018-12-30 16:12:15 +0530

    Linked html file with vue js and testing it

---
+++ script.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        product: "Socks",
+        product_description: "It is made of fabric and it has all the necessary stiches to make it perfect."
+    }
+})
