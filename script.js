18d709e4d84b02947b3b30ef8df28c491a42941b
CompositionInJava <godabavinay@gmail.com>
2018-12-30 17:30:57 +0530

    Added cart and variants variables

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ var app = new Vue({
     data: {
         product: "Socks",
         product_description: "It is made of fabric and it has all the necessary stiches to make it perfect.",
-        inventory: 8
+        inStock: true,
+        details: ["80% cotton", "20% neutral", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        cart: 0
     }
 })
